1529fc4f9e3df8f06c9fdc03689f9ab1a61b13d9
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-12-16 22:39:05 +0600

    setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import { NavLink, Link } from 'react-router-dom';
 const Header = () => {
     return (
-        <div>
-            <h1>Header </h1>
-        </div>
+        <>
+            <header className="header top strip py-3">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-6">
+                            <p>Free Shopping Over $100 & Free Returns</p>
+                        </div>
+                        <div className="col-6">
+                            <p className='text-end'>Hotline: <a href="tel:+8801538088262"> +88 01538088262</a></p>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+        </>
     );
 };
 
